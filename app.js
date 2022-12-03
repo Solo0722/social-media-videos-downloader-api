4d6cf2f4fb469a426407f9399ea46bb70133642d
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2022-12-03 00:33:20 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const downloader = require("./routes/downloader");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", downloader);
+
+
+app.listen(2020);
+
+module.exports = app;
